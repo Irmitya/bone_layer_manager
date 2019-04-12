6aa6df2866b2e508e4192174cbef79470cb8f9f0
fineskimo <fineskimo@users.noreply.github.com>
2019-04-12 14:06:44 +0100

    Added Shit+Click to make button row creation easier
    I tried ro do this with getattr() and setattr() but just couldn't get it to work..

--- create_rigui_id.py
+++ create_rigui_id.py
@@ -18,15 +18,36 @@ class SETUIID_OT_riguiid(bpy.types.Operator):
                           description="Index of the RigUI layer",
                           default=0, min=0, max=31, soft_min=0, soft_max=31)
 
+    rigui_idcount: IntProperty(name="RigUI Count",
+                               description="RigUI layer Counter",
+                               default=0, min=0, max=31, soft_min=0, soft_max=31)
+
     @classmethod
     def poll(self, context):
         arm = getattr(context.active_object, 'data', None)
         not_link = (getattr(arm, 'library', None) is None)
         return not_link
 
-    def execute(self, context):
+    def invoke(self, context, event):
         arm = bpy.context.active_object.data
-        # Create ID prop by setting it
-        arm[f"rigui_id_{self.layer_idx}"] = self.rigui_id
+
+        try:
+            count = arm['rigui_idcount']
+        except KeyError:
+            arm['rigui_idcount'] = self.rigui_idcount
+
+        count = arm['rigui_idcount']
+        # Use same rigui_id
+        if event.shift:
+            # Create ID prop by setting it
+            arm[f"rigui_id_{self.layer_idx}"] = self.rigui_id
+            arm[f"rigui_id_{self.layer_idx}"] = count
+        # Increment rigui_id
+        else:
+            # Create ID prop by setting it
+            count += 1
+            arm['rigui_idcount'] = count
+            arm[f"rigui_id_{self.layer_idx}"] = self.rigui_id
+            arm[f"rigui_id_{self.layer_idx}"] = count
 
         return {'FINISHED'}
