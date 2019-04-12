758dce241aaa2da72484c621fb3a710faee17575
fineskimo <fineskimo@users.noreply.github.com>
2019-04-12 14:03:32 +0100

    Change rigui_id to set in operator
    The current way gave odd results as it copied the layer idx value... it didn't increment correctly.

--- bone_layers_panel.py
+++ bone_layers_panel.py
@@ -170,7 +170,9 @@ class BLM_PT_panel_layers(bpy.types.Panel):  # renamed as now is subpanel of BLM
                                  toggle=True, text="")
 
                     # RigUI Setup fields
+
                     if prefs().BLM_ShowRigUI:
+
                         if rigui_id is not None:
                             # row.prop(ac_ob.data, f'["{rigui_id_prop}"]', index=i, text="UI Layer ")
                             if (rigui_id > 0) and (rigui_id < 33):
@@ -182,7 +184,8 @@ class BLM_PT_panel_layers(bpy.types.Panel):  # renamed as now is subpanel of BLM
                         else:
                             id_op = row.operator("bone_layer_man.rigui_set_id")
                             id_op.layer_idx = i
-                            id_op.rigui_id = i + 1
+                            # id_op.rigui_id = i+1  #Set in operator instead
+
                             if ac_ob.data.get(rigui_id_prop):
                                 row.prop(
                                     ac_ob.data, f'["{rigui_id_prop}"]', index=i, text="")
