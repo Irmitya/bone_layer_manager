e9ba5562a0a84a9192963443b5cbf485acc18dc4
fineskimo <fineskimo@users.noreply.github.com>
2019-08-06 12:18:50 +0100

    Clearer names\variables

--- Example Enum Property.py
+++ Example Enum Property.py
@@ -3,15 +3,15 @@ import bpy
 def update_enum(self, context):
     print(self, self.name, context)
 
-bpy.types.PoseBone.test_enum = bpy.props.EnumProperty(
+bpy.types.PoseBone.propname= bpy.props.EnumProperty(
     items=(
     ('state_1', 'Name 1','Tooltip_1'),
     ('state_2', 'Name 2','Tooltip_2'),
     ('state_3', 'Name 3', 'Tooltip_3'),
     ),
-    name="Property",
+    name="Set Property",
     default="state_1",
     update=update_enum
     )
 
-bpy.data.objects["Armature"].pose.bones["Bone"].test_enum = "state_1"
+bpy.data.objects["Armature"].pose.bones["Bone"].propname = "state_1"
