e133a0c48ffb0063d687ce4df1295dd2b7072734
fineskimo <fineskimo@users.noreply.github.com>
2019-09-19 13:07:43 +0100

    fix : text values inverted

--- bone_layers_panel.py
+++ bone_layers_panel.py
@@ -93,7 +93,7 @@ class BLM_PT_panel_layers(bpy.types.Panel):  # renamed as now is subpanel of BLM
         row = layout.row()
 
         row.operator("bone_layer_man.deformtoggle", emboss=True,
-                     text=f"Toggle Deform({('ON', 'OFF')[is_deform]})")
+                     text=f"Toggle Deform({('OFF', 'ON')[is_deform]})")
 
         row.operator("bone_layer_man.deformerisolate",
                      emboss=True, text="Deform Bones Only")
